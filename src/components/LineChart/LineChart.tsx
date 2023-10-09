b9cdf9d11ad00e08ad726ade68704519c74fa40e
David <davidmchung123@gmail.com>
2023-10-09 10:19:31 -0500

    revert
    * I'll figure this out later.

--- src/components/LineChart/LineChart.tsx
+++ src/components/LineChart/LineChart.tsx
@@ -254,10 +254,9 @@ function mouseMove(data, selectedData, x, y, scaleX, format, marginLeft, marginR
     }
 
     const toolTip = d3.select(document.getElementById("tooltip-" + toolId));
-
-    toolTip.style("visibility", "visible")
-            .style("top", d.clientY + "px")
-            .style("left", d.clientX + "px");
+        toolTip.style("visibility", "visible")
+            .style("top", (coords[1]) + "px")
+            .style("left", (coords[0] + 25) + "px");
 
     for (let i = 0; i < selectedData.length; i++) {
         if (selectedData[i].isSelected) {
